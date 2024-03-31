61127aa4a7e211b8955fa04182ef989ac8725e43
Avadhut-Mypcot <avdhut.k@mypcot.com>
2024-03-31 14:44:23 +0530

    revert commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const connectDatabase = require("./config/database");
 const cloudinary = require('cloudinary');
 dotenv.config();
-app.use(cors(corsOptions));
+
 //handling uncaught exception
 process.on('uncaughtException', (err) => {
     console.log(`Uncaught Exception: ${err}`);
